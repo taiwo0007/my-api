9ec75ca65a10bdd146ec35faf9bf26c7028755f2
taiwo <X00167646@myTUDublin.ie>
2022-12-20 10:07:09 +0000

    first commit

---
+++ src/test/java/com/myapi/demo/myapi/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapi.demo.myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
